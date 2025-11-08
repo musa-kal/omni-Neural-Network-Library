e3318dd9811765fee36e8305ac1f1414067d5d22
Musa Kaleem <131704022+Musa-Kal@users.noreply.github.com>
2025-11-08 17:15:35 -0500

    repr added to layers class

--- model.py
+++ model.py
@@ -26,20 +26,28 @@ class Layers:
     def __init__(self, first_layer: BaseLayer, activation_function: str=None):
         self.layers = [first_layer]
         self.weights = []
-        self.activation_function = activation_function if activation_function else None
+        self.activation_function = [activation_function if activation_function else None]
     
-    def join_front(self, new_layer: BaseLayer):
-        if len(new_layer.shape) == len(self.layers[-1].shape):
+    def join_front(self, new_layer: BaseLayer, activation_function: str=None):
+        if len(new_layer.shape) != len(self.layers[-1].shape):
             raise ValueError("new layer shape doesn't match the previous layer!")
         self.layers.append(new_layer)
         self.weights.append(np.empty(shape=(new_layer.shape[0], self.layers[-1].shape[0])))
+        self.activation_function.append(activation_function)
 
 
-    
+    def __repr__(self):
+        output = "layers: ["
+        for layer in self.layers:
+            output += f"{layer.shape}\t"
+        return output.strip() + "]"
     
     
     
 
 
 if __name__ == '__main__':
+    x = Layers(DenseLayer(1))
+    x.join_front(DenseLayer(3))
+    print(x)
     pass
