4c5646f0086c92c3610f868dd3788ffcfdd1b32b
Musa Kaleem <131704022+Musa-Kal@users.noreply.github.com>
2025-11-11 17:06:19 -0500

    code refactor and requirements.txt added

--- code/model.py
+++ code/model.py
@@ -1,8 +1,15 @@
+"""
+Author: Musa Kaleem
+"""
+
 import numpy as np
 
 NP_FLOAT_PRECISION = np.float32
 
 class ActivationFunctions:
+    """
+    Activation Functions class will hold different pre built activation function.
+    """
     pass
 
 # class Neuron:
@@ -13,24 +20,41 @@ class ActivationFunctions:
 #         return z + self.bias
 
 class Layers:
+    """
+    Layers class holds logic for joining and infracting with joint layers and some other helper classes.
+    """
+
     class BaseLayer:
         """
-        Base Layer Class used to create different types of hidden layers
+        Base Layer class used to create different types of hidden layers
         """
         def __init__(self):
             self.shape = ()
             self.name = None
 
         def feedforward(self):
-            raise NotImplementedError("feedforward must be implemented in the child class!")
+            """
+            should return a numpy array
+            """
+            raise NotImplementedError(f"feedforward must be implemented in the child class {self.name}!")
         
         def feedbackwards(self):
-            raise NotImplementedError("feedbackwards must be implemented in the child class!")
+            """
+            still figuring out how the returns should work
+            """
+            raise NotImplementedError(f"feedbackwards must be implemented in the child class {self.name}!")
         
         def __repr__(self):
+            """
+            return string including the layer name and shape
+            """
             return f"{self.name}-{self.shape}"
 
+
     class DenseLayer(BaseLayer):
+        """
+        Dense Layer class inherits from BaseLayer and represents a dense layer
+        """
         def __init__(self, neuron_count:int=1, activation_function:ActivationFunctions|None=None, input_shape:tuple=(1,)):
             if neuron_count < 1:
                 raise ValueError("neuron_count must be greater then 0!")
@@ -43,7 +67,11 @@ class Layers:
             self.post_activation = None
             self.prev_input = None
         
+
         def feedforward(self, input_array, save=False):
+            """
+            feeds the input data forward through layer and returns the output a numpy array
+            """
             if input_array.shape[0] != self.weights.shape[1]:
                 raise ValueError(f"Input array shape {input_array.shape} doesn't match the shape of the layers weights shape {self.weights.shape}")
             
@@ -62,7 +90,12 @@ class Layers:
 
             return z
         
+
         def feedbackwards(self, lose_derivatives):
+            """
+            accepts a lose_derivatives which is ∂L/∂a derivative of lose function with respect to the 
+            activation of current layer and returns ∂L/∂a(L-1), ∂L/∂w, ∂L/∂b
+            """
             if lose_derivatives.shape != self.shape:
                 raise ValueError(f"lose_derivatives {lose_derivatives.shape} doesn't equal current layer shape {self.shape}")
             
@@ -80,7 +113,10 @@ class Layers:
             return np.dot(self.weights.T, dL_dz), dz_dw.T * dL_dz, dL_dz
 
             
-        def clear_all_saves(self):
+        def clear_all_saved_data(self):
+            """
+            helper function to clear all saved data within the current layer
+            """
             self.pre_activation = None
             self.post_activation = None
             self.prev_input = None
@@ -91,6 +127,9 @@ class Layers:
         self.layers = []        
 
     def join_front(self, new_layer: BaseLayer):
+        """
+        takes a BaseLayer class and joins it to the front/right of the network.
+        """
 
         if len(self.layers) == 0:
             if len(new_layer.shape) != len(self.input_shape):
@@ -102,32 +141,35 @@ class Layers:
             raise ValueError(f"new layer shape {new_layer.shape} doesn't match the previous layer {self.layers[-1].shape}!")
         self.layers.append(new_layer)
 
+
     def feedforward(self, input_data):
+        """
+        takes input data and feeds it through the network returning the output.
+        """
         if input_data.shape != self.input_shape:
             raise ValueError(f"input_data shape {input_data.shape} doesn't match the shape specified f{self.input_shape}")
         
-        layer_outputs = []
 
         next_input = input_data
         for i, layer in enumerate(self.layers):
             curr_output = layer.feedforward(next_input)
-            layer_outputs.append(curr_output)
             next_input = curr_output
         
-        return layer_outputs
+        return next_input
 
     def propagate_backwards(self, layer_outputs):
         pass
 
     def __repr__(self):
+        """
+        string representing the network.
+        """
         output = f"layers: [Input-{self.input_shape}\t" 
         for layer in self.layers:
             output += f"{layer}\t"
         return output.strip() + "]"
     
     
-    
-
 
 if __name__ == '__main__':
     x = np.array([
