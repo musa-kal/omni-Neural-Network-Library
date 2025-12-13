40d9dc9afdd47577669d8b550c4b5e6d2aca937e
Musa Kaleem <131704022+Musa-Kal@users.noreply.github.com>
2025-12-13 14:30:04 -0500

    test_run.py simplified

--- test_run.py
+++ test_run.py
@@ -15,30 +15,9 @@ model = Model(x)
 model.compile(loss_function=model.MSE)
 model.fit(X, y, epoch=300, batch_size=16)
 
-from tensorflow import keras
-
-tmodel = keras.Sequential([
-    keras.layers.Dense(64, activation='relu', input_shape=(1,)),
-    keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(1)
-])
-
-tmodel.compile(
-    optimizer=keras.optimizers.Adam(learning_rate=0.01),
-    loss='mse'
-)
-
-tmodel.fit(
-    X, y,
-    epochs=300,
-    batch_size=16
-)
-
-
 import matplotlib.pyplot as plt
 plt.scatter(X, y, color='blue', label='Data Points: 100 * (X - 1) ** 2 + ε')
 plt.scatter(X, tuple(model.predict(x) for x in X), color='red', label='Omni fit line')
-plt.scatter(X, tmodel.predict(X), color='green', label='TF fit line')
 plt.xlabel('X')
 plt.ylabel('y')
 plt.legend()
